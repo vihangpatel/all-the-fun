5c24c649c7aa71d48a55cc5a9c7d939f7e6f5ad3
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2018-10-28 15:38:08 +0530

    fix css issue

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express')
+const app = express()
+const helmet = require('helmet')
+const path = require('path')
 
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(helmet())
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
 
 app.get('/speakers', function(req, res) {
-    res.sendFile(path.join(__dirname + '/speakers.html'));
-});
+  res.sendFile(path.join(__dirname + '/public/speakers.html'))
+})
 
-app.listen(8080);
+app.listen(8080, () => console.log('serving at http://localhost:8080'))
