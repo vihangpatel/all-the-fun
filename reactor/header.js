68fc442bc6256edf3ddc9098368ac2ba11e078eb
Manjula Dube <dube.manjula668@gmail.com>
2018-10-28 15:09:46 +0100

    yoo

--- reactor/header.js
+++ reactor/header.js
@@ -22,26 +22,10 @@ const Header = () => (
 
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav ml-auto'>
-            <li className='nav-item dropdown'>
-              <a
-                className='nav-link dropdown-toggle'
-                href='#'
-                id='navbarDropdown2'
-                role='button'
-                data-toggle='dropdown'
-                aria-haspopup='true'
-                aria-expanded='false'
-                            >
+            <li className='nav-item'>
+              <a className='nav-link' href='/about'>
                                 Home
                             </a>
-              <div className='dropdown-menu' aria-labelledby='navbarDropdown2'>
-                <a className='dropdown-item' href='/home'>
-                                    Home One
-                                </a>
-                <a className='dropdown-item' href='index2.html'>
-                                    Home Two
-                                </a>
-              </div>
             </li>
             <li className='nav-item'>
               <a className='nav-link' href='/about'>
