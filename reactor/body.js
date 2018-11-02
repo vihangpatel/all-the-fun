22f0ddf7a6d64d6e816e1f3170e1422771d410b8
Vihang Patel <vihang.engg@gmail.com>
2018-11-02 21:25:49 +0530

    Default show homepage

--- reactor/body.js
+++ reactor/body.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import Header from './header'
-import Footer from './footer'
-import Home from './pages/home'
-import About from './pages/about'
-import Speakers from './pages/speakers'
-import ContactUs from './pages/contactus'
-import Sponsorship from './pages/sponsorship'
+import React from "react"
+import Header from "./header"
+import Footer from "./footer"
+import Home from "./pages/home"
+import About from "./pages/about"
+import Speakers from "./pages/speakers"
+import ContactUs from "./pages/contactus"
+import Sponsorship from "./pages/sponsorship"
 
 const pageMap = {
 	home: Home,
 	about: About,
 	speakers: Speakers,
-	'contact-us': ContactUs,
+	"contact-us": ContactUs,
 	sponsorship: Sponsorship,
 }
 
 const DefaultPage = () => <h1> Not Found</h1>
 
 const getPage = pageName => {
-	const Page = pageMap[pageName] || DefaultPage
+	const Page = pageMap[pageName] || Home
 	return <Page />
 }
 
