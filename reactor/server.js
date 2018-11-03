26afee9f578db141580ddb28ce81edeacc9ebcdf
Vihang Patel <vihang.engg@gmail.com>
2018-11-03 11:04:55 +0530

    Remove unwanted code

--- reactor/server.js
+++ reactor/server.js
@@ -5,21 +5,14 @@ const path = require("path")
 
 const reactor = require("./index")
 
-console.log(__dirname)
-
 app.use(helmet())
 app.use(express.static(path.join(__dirname, "..", "public")))
 
 const requestHandler = (req, res) => reactor.renderToString(req, res)
 
-app.get("/", requestHandler)
-app.get("/(:pageName)", requestHandler)
+app.get("*", requestHandler)
 
-app.get("/speakers", function(req, res) {
-	res.sendFile(path.join(__dirname + "/public/speakers.html"))
-})
-
-const server = app.listen(process.env.PORT || 8081, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
 	const host = server.address().address
 	const port = server.address().port
 	console.log(`serving at ${host} & port : ${port}`)
