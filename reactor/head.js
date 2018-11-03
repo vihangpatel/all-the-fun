77585b825a0fc4433075937504ec79b3062af7b7
Vihang Patel <vihang.engg@gmail.com>
2018-11-03 10:03:26 +0530

    Add SEO

--- reactor/head.js
+++ reactor/head.js
@@ -6,8 +6,26 @@ const Head = () => (
 		<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0" />
 		<meta name="keywords" content="" />
-		<meta name="description" content="" />
-		<title>VenToT - Conference, Seminar & Event HTML Template</title>
+		<meta name="description" content={description} />
+		<link rel="apple-touch-icon" sizes="57x57" href="assets/favico/apple-icon-57x57.png" />
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/favico/apple-icon-60x60.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/favico/apple-icon-72x72.png" />
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/favico/apple-icon-76x76.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/favico/apple-icon-114x114.png" />
+		<link rel="apple-touch-icon" sizes="120x120" href="assets/favico/apple-icon-120x120.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/favico/apple-icon-144x144.png" />
+		<link rel="apple-touch-icon" sizes="152x152" href="assets/favico/apple-icon-152x152.png" />
+		<link rel="apple-touch-icon" sizes="180x180" href="assets/favico/apple-icon-180x180.png" />
+		<link rel="icon" type="image/png" sizes="192x192" href="assets/favico/android-icon-192x192.png" />
+		<link rel="icon" type="image/png" sizes="32x32" href="assets/favico/favicon-32x32.png" />
+		<link rel="icon" type="image/png" sizes="96x96" href="assets/favico/favicon-96x96.png" />
+		<link rel="icon" type="image/png" sizes="16x16" href="assets/favico/favicon-16x16.png" />
+		<link rel="manifest" href="assets/favico/manifest.json" />
+		<meta name="msapplication-TileColor" content="#ffffff" />
+		<meta name="msapplication-TileImage" content="assets/favico/ms-icon-144x144.png" />
+		<meta name="theme-color" content="#ffffff" />
+
+		<title>React India - 2019</title>
 
 		<link rel="shortcut icon" type="image/png" href="assets/images/favicon.png" />
 
@@ -27,13 +45,18 @@ const Head = () => (
 
 		<link rel="stylesheet" type="text/css" href="assets/css/responsive.css" />
 
-		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"/>
-		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400i" rel="stylesheet"></link>
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" />
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400i" rel="stylesheet" />
 
 		<script src="assets/js/modernizr.custom.js" />
 
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js" />
 	</head>
 )
+
+const description = `Do you want to learn about React and React Native?
+Then you shouldn't miss React India - Goa next August! 
+You will have the opportunity to improve your developer skills and meet people from around the world, 
+while enjoying the beaches of Goa`
 
 export default Head
