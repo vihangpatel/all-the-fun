843b656f86c858eb7e2111821393b6d20c1effe4
Manjula Dube <dube.manjula668@gmail.com>
2018-11-04 12:39:07 +0100

    add ken wheeler

--- reactor/data/speakers.js
+++ reactor/data/speakers.js
@@ -1,35 +1,32 @@
 const data = [
-	{
-		name: "Sunil Pai",
-		imgPath: "sunil_pai.jpeg", // with extension
-		github: "https://github.com/manjula91",
-		twitter: "https://twitter.com/manjula_dube",
-		facebook: "https://www.facebook.com/manjuladube",
-		medium: "https://medium.com/@manjuladube",
-		description: "Coding enthu, Cook, Organizes React India - 2019", // keep it simple
-		jobrole: "Mad Scientist, Facebook", // only one line
-	},
-	{
-		name: "Sara Vieria",
-		imgPath: "sara_vieria.jpg",
-		github: "https://github.com/manjula91",
-		twitter: "https://twitter.com/manjula_dube",
-		facebook: "https://www.facebook.com/manjuladube",
-		medium: "https://medium.com/@manjuladube",
-		description:
-			"Developer Advocate at @YLDio. GraphQL and Open Source enthusiast. Conference Speaker and Airport expert. I am also into drums and horror movies.",
-		jobrole: "YLDio, Europe",
-	},
-	{
-		name: "Manjula Dube",
-		imgPath: "manjula_dube.jpg",
-		github: "https://github.com/manjula91",
-		twitter: "https://twitter.com/manjula_dube",
-		facebook: "https://www.facebook.com/manjuladube",
-		medium: "https://medium.com/@manjuladube",
-		description: "Coding enthu, Cook, Organizes React India - 2019",
-		jobrole: "Ex Sr. Dev@BookMyShow",
-	},
+  {
+    name: 'Sunil Pai',
+    imgPath: 'sunil_pai.jpeg', // with extension
+    github: 'https://github.com/manjula91',
+    twitter: 'https://twitter.com/manjula_dube',
+    facebook: 'https://www.facebook.com/manjuladube',
+    medium: 'https://medium.com/@manjuladube',
+    description: 'Coding enthu, Cook, Organizes React India - 2019', // keep it simple
+    jobrole: 'Mad Scientist, Facebook' // only one line
+  },
+  {
+    name: 'Sara Vieria',
+    imgPath: 'sara_vieria.jpg',
+    github: 'https://github.com/manjula91',
+    twitter: 'https://twitter.com/manjula_dube',
+    facebook: 'https://www.facebook.com/manjuladube',
+    medium: 'https://medium.com/@manjuladube',
+    description: 'Developer Advocate at @YLDio. GraphQL and Open Source enthusiast. Conference Speaker and Airport expert. I am also into drums and horror movies.',
+    jobrole: 'YLDio, Europe'
+  },
+  {
+    name: 'Ken Wheeler',
+    imgPath: 'ken_wheeler.jpeg',
+    github: 'https://github.com/kenwheeler',
+    twitter: 'https://twitter.com/ken_wheeler',
+    description: 'Author of libraries like Slick Carousel, McFly, react-music, webpack-dashboard, Spectacle.',
+    jobrole: 'Code, beats and meat - Google Developer Expert'
+  }
 ]
 
 export default data
