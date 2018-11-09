28690bd91ca1684d2acaae80a52485a589751a7d
Manjula Dube <dube.manjula668@gmail.com>
2018-11-09 11:00:56 +0100

    data change

--- reactor/pages/home.js
+++ reactor/pages/home.js
@@ -50,7 +50,7 @@ class Home extends Component {
 							<div className="caption__small">
 								<p className="caption__small--para event-date">
 									<i className="fa fa-calendar" />
-									23rd - 25th August, 2019.
+									22rd - 24th August, 2019.
 								</p>
 								<p className="caption__small--para uppercase event-location">
 									<i className="flaticon-location-pointer" />
