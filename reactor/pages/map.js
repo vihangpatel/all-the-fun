7a59e4db69617b4808e947b9b024e6d62fa96a58
Vihang Patel <vihang.engg@gmail.com>
2018-11-14 19:14:35 +0530

    Add maps exactly where Manjula wants it :) :)

--- reactor/pages/map.js
+++ reactor/pages/map.js
@@ -1,38 +1,37 @@
 export const initMap = () => {
 	const position = {
-		lat: 15.317396,
-		lng: 73.902374,
+		lat: 15.316871,
+		lng: 73.901903
 	}
 	// Styles a map in night mode.
-	var map = new google.maps.Map(document.getElementById("map"), {
+	const map = new google.maps.Map(document.getElementById('map'), {
 		center: position,
-		zoom: 15,
+		zoom: 15
 	})
 	// Let's also add a marker while we're at it
-	var marker = new google.maps.Marker({
+	const marker = new google.maps.Marker({
 		position,
-		map: map,
-		animation: google.maps.Animation.DROP,
+		map,
+		animation: google.maps.Animation.DROP
 	})
 }
 
 export const addMap = () => {
-	return false
 	addScript({
 		async: true,
 		defer: true,
-		src: "//maps.googleapis.com/maps/api/js?key=AIzaSyA-OJo_BS_LEeMdzIwq30RE7ZUnE6uuDW4",
-		onload: initMap,
+		src: '//maps.googleapis.com/maps/api/js?key=AIzaSyA-OJo_BS_LEeMdzIwq30RE7ZUnE6uuDW4',
+		onload: initMap
 	})
 }
 
 const addScript = options => {
-	var s = document.createElement("script") // Create a script element
-	s.type = "text/javascript" // optional in html5
+	const s = document.createElement('script') // Create a script element
+	s.type = 'text/javascript' // optional in html5
 	s.async = options.async // asynchronous? true/false
 	s.defer = options.defer
 	s.src = options.src
 	s.onload = options.onload
-	var fs = document.getElementsByTagName("script")[0] // Get the first script
+	const fs = document.getElementsByTagName('script')[0] // Get the first script
 	fs.parentNode.insertBefore(s, fs)
 }
