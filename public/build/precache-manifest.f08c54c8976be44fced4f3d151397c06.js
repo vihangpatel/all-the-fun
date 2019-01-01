6c6a588d9cb3edc71b36bbc5e1a69ddfc5f5e65b
Vihang Patel <vihang.engg@gmail.com>
2019-01-01 10:27:36 +0530

    Rebuild code

---
+++ public/build/precache-manifest.f08c54c8976be44fced4f3d151397c06.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "da2ca861def4a68b42cf",
+    "url": "/build/vendor.29b647ba3e1a92c6348e.js"
+  },
+  {
+    "revision": "962047d45512ac78aeacf177c8608a4a",
+    "url": "/build/stats.json"
+  },
+  {
+    "revision": "363a4719bd018fc814d9",
+    "url": "/build/client.061a6f2f3fe06a87f16f.js"
+  }
+];
