71bc6774bbecc9732d3ebf20cd82df0dc2887592
Manjula Dube <dube.manjula668@gmail.com>
2019-02-15 22:48:24 +0100

    doneneee

---
+++ public/build/precache-manifest.d10853293d813dca4dfc43cb21bca48f.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "086c1793c07d5d436bd9",
+    "url": "/build/vendor.c610868010581ed5552e.js"
+  },
+  {
+    "revision": "fa2feec2cf15ddccb6029c57a44c08ed",
+    "url": "/build/stats.json"
+  },
+  {
+    "revision": "14b5c1541d0060de741e",
+    "url": "/build/client.f06dc1ec888b4a8dd908.js"
+  }
+];
