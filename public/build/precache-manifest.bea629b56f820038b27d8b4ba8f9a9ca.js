7d447ff27b3e542931515c168981e125296e1ed9
Manjula Subhashchandra Dube <dube.manjula668@gmail.com>
2019-03-20 14:16:33 +0100

    full access shared to single

---
+++ public/build/precache-manifest.bea629b56f820038b27d8b4ba8f9a9ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39b87047977a6a260146",
+    "url": "/build/vendor.1cba30644f570d85128c.js"
+  },
+  {
+    "revision": "947dad05ffcb126e4c38",
+    "url": "/build/style.ffc5a0f59d3a10ca882e.js"
+  },
+  {
+    "revision": "947dad05ffcb126e4c38",
+    "url": "/build/style.c013bf9b482dd20ac00b.css"
+  },
+  {
+    "revision": "84707649a01fd10d60f49acfd1fe7418",
+    "url": "/build/stats.json"
+  },
+  {
+    "revision": "eed64dd2a17dc4238035",
+    "url": "/build/client.7fc43391daadb119758c.js"
+  }
+];
