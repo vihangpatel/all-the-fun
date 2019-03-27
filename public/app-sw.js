9b5632a3731f4f3dbfeb17db57b285be684c7d79
Manjula Subhashchandra Dube <dube.manjula668@gmail.com>
2019-03-27 23:13:07 +0100

    Our Mcs added

--- public/app-sw.js
+++ public/app-sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.947c11102cc005ab4449a8f4bab11482.js"
+  "/build/precache-manifest.cd4a25242951b3398d78dbde28aed80a.js"
 );
 
 workbox.skipWaiting();
---
+++ public/build/precache-manifest.cd4a25242951b3398d78dbde28aed80a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce13cdc0e0f91c0d9df3",
+    "url": "/build/vendor.969b047d1315d7c55f1c.js"
+  },
+  {
+    "revision": "1e16da19de046327d20c",
+    "url": "/build/style.94ed305679303e0da2dc.css"
+  },
+  {
+    "revision": "1e16da19de046327d20c",
+    "url": "/build/style.835ee23dfc2cd976c1b6.js"
+  },
+  {
+    "revision": "6970920d1c4a100fd87607cd8ec1e50d",
+    "url": "/build/stats.json"
+  },
+  {
+    "revision": "a6bd66b8478a7c346c35",
+    "url": "/build/client.adba46a14544ffe27495.js"
+  }
+];
