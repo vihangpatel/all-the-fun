e055a558d19d6176f7e071d02e72b27e8253bd25
Anjali Dube <anjalidube@Anjalis-MBP.fritz.box>
2019-07-14 21:20:52 +0200

    footer background changed (banner)

--- public/app-sw.js
+++ public/app-sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.8face3c7b9dbc19dcf321d3086dd66c3.js"
+  "/build/precache-manifest.9f7efaf2c2b2c2d65ae8ab475db599f1.js"
 );
 
 workbox.skipWaiting();
