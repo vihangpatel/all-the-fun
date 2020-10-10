935e0e985edff4378615ef982188e23888ec4bfc
vihangpatel <vihang.engg@gmail.com>
2020-10-10 16:30:58 +0530

    Kill port to exit build process

--- snapshot-generator.sh
+++ snapshot-generator.sh
@@ -1,11 +1,13 @@
 yarn
+yarn kill
 yarn build
 PORT=7070 yarn start &
 echo "Server has started ... Let it spin at full speed"
 echo "\n****"
-echo "Waiting for 10 seconds ..."
-sleep 10s
+echo "Waiting for few seconds ..."
+sleep 3s
 echo "Requesting localhost and saving snapshot"
 curl http://localhost:7070 -o public/index.html
 echo "Done !!!\n"
+yarn kill
 exit
