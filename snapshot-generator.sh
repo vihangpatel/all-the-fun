02eb27bacce22d3b0fc4ed53b7c64036cb690f01
vihangpatel <vihang.engg@gmail.com>
2020-10-10 16:24:41 +0530

    Update build command

--- snapshot-generator.sh
+++ snapshot-generator.sh
@@ -1,7 +1,11 @@
 yarn
 yarn build
-yarn kill
 PORT=7070 yarn start &
+echo "Server has started ... Let it spin at full speed"
+echo "\n****"
+echo "Waiting for 10 seconds ..."
 sleep 10s
+echo "Requesting localhost and saving snapshot"
 curl http://localhost:7070 -o public/index.html
-yarn kill
+echo "Done !!!\n"
+exit
