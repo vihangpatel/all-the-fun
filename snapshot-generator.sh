258142764637179135ad8c7f721fbd30984fee95
vihangpatel <vihang.engg@gmail.com>
2020-10-10 16:16:37 +0530

    Fix snap shoter file

--- snapshot-generator.sh
+++ snapshot-generator.sh
@@ -2,5 +2,6 @@ yarn
 yarn build
 yarn kill
 PORT=7070 yarn start &
-sleep 15s
-curl -K http://localhost:7070 -o public/index.html
+sleep 10s
+curl http://localhost:7070 -o public/index.html
+yarn kill
