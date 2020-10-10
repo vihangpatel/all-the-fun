da8c09b93b4c54e78890371ebc3f56b9ee114c22
vihangpatel <vihang.engg@gmail.com>
2020-10-10 16:34:47 +0530

    Prebuilt code

---
+++ public/build/precache-manifest.5342d73734fb6e39436ff169f8dc9b9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa9465531a84e1827f7b",
+    "url": "/build/client.0e44f881ae9e7a32d3cf.js"
+  },
+  {
+    "revision": "7fb606f5f9e9e4c8eaee",
+    "url": "/build/style.f21a1af35516c0c4efdf.css"
+  },
+  {
+    "revision": "7fb606f5f9e9e4c8eaee",
+    "url": "/build/style.16d04a8b540910905c3d.js"
+  },
+  {
+    "revision": "9ba68b2761387cdac8be",
+    "url": "/build/vendor.88be5d17c6809e4009db.js"
+  },
+  {
+    "revision": "2ff2f8528965a1cfb8943b3279a90ad6",
+    "url": "/build/stats.json"
+  }
+];
